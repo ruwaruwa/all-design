9442c654219942f61145217d498a238b2a65459e
ruwaruwa <almozali287@gmail.com>
2023-11-27 01:02:44 -0800

    reset

--- en.js
+++ en.js
@@ -1,13 +1,38 @@
 const encrement=document.querySelector('#encrement')
 const resetbutton= document.querySelector('#reset')
 const h1=document.querySelector('h1')
-let startvalue=0
+const decrement=document.querySelector('#decre')
+
+let startvalue=0;
+ let endvalue=0
+
 encrement.addEventListener('click',function(){
     startvalue++;
 h1.innerHTML=startvalue
+if(startvalue ==10){
+    h1.style.color="red"
+}
+else{
+    h1.style.color="black"
+}
 })
-resetbutton.addEventListener('click',function(){
+decrement.addEventListener('click',function(){
+    // startvalue--;
+
+    // h1.innerHTML=startvalue;
+    // h1.innerHTML=-0;
+if(startvalue > 0){
     startvalue--;
-h1.innerHTML=startvalue
-h1.innerHTML=0
+h1.innerHTML=startvalue;
+
+}
+
+
 })
+
+resetbutton.addEventListener('click',function(){
+     startvalue--;
+h1.innerHTML=startvalue
+ h1.innerHTML=0
+})
+
