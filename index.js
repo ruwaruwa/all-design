27cf491755296476da8a36e009c70ab559f9739c
ruwaruwa <almozali287@gmail.com>
2023-11-26 23:55:02 -0800

    testd

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const iconbutons =document.querySelector('#iconButton')
 const links =document.querySelector('#links')
+const closeutton=document.querySelector('#closebutton')
+
 iconbutons.addEventListener('click',function(){
     links.style.display = 'flex'
+    closeutton.style.display="block"
+    iconbutons.style.display="none"
+})
+
+closeutton.addEventListener('click',function(){
+    links.style.display='none'
+    iconbutons.style.display='block'
+    closeutton.style.display="none"
 })
