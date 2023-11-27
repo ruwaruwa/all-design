ab3e3afaecf722add7372fa206f053ec313cce53
ruwaruwa <almozali287@gmail.com>
2023-11-26 23:03:04 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const iconbutons =document.querySelector('#iconButton')
+const links =document.querySelector('#links')
+iconbutons.addEventListener('click',function(){
+    links.style.display = 'flex'
+})
